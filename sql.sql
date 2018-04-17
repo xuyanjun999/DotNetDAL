50e98070962f1b277a98b1fec51026bc0815875e
wangchengqun <373092793@qq.com>
2018-04-17 11:23:34 +0800

    Create sql.sql

---
+++ sql.sql
@@ -0,0 +1,18 @@
+
+CREATE DATABASE Test1
+go
+use Test1
+CREATE TABLE test0(
+	Id int NOT NULL,
+	Name nvarchar(50) NULL,
+	Address nvarchar(50) NULL
+) 
+
+CREATE DATABASE Test2
+go
+use Test2
+CREATE TABLE test0(
+	Id int NOT NULL,
+	Name nvarchar(50) NULL,
+	Address nvarchar(50) NULL
+) 
